151dbada1f6348b85f0f9655940ceeab9e40d37b
Nicolas Orellana <nicolas.orellana@vivint.com>
2024-07-24 09:52:15 -0400

    updated design and validations

--- SacramentMeetingPlanner/Models/SacramentProgram.cs
+++ SacramentMeetingPlanner/Models/SacramentProgram.cs
@@ -10,49 +10,49 @@ public class SacramentProgram
     [DataType(DataType.Date)]
     public DateTime MeetingDate { get; set; }
     [Required]
-    [StringLength(40)]
+    [StringLength(60)]
     [Display(Name = "Conducting Leader")] 
     public string? ConductingLeader { get; set; }
     [Required]
-    [StringLength(30)]
+    [StringLength(60)]
     [Display(Name = "Opening Song")]
     public string? OpeningSong { get; set; }
     [Required]
-    [StringLength(40)]
+    [StringLength(60)]
     [Display(Name = "Opening Prayer")] 
     public string? OpeningPrayer { get; set; }
     [Required]
-    [StringLength(30)]
+    [StringLength(60)]
     [Display(Name = "Sacrament Hymn")] 
     public string? SacramentHymn { get; set; }
     [Required]
-    [StringLength(40)]
+    [StringLength(60)]
     [Display(Name = "1st Speaker")] 
     public string? Speaker1 { get; set; }
     [Required]
-    [StringLength(30)]
+    [StringLength(60)]
     [Display(Name = "Subject")] 
     public string? Speaker1Subject { get; set; }
     [Required]
-    [StringLength(40)]
+    [StringLength(60)]
     [Display(Name = "2nd Speaker")] 
     public string? Speaker2 { get; set; }
     [Required]
-    [StringLength(30)]
+    [StringLength(60)]
     [Display(Name = "Subject")]
     public string? Speaker2Subject { get; set; }
-    [StringLength(40)]
+    [StringLength(60)]
     [Display(Name = "3rd Speaker")]
     public string? Speaker3 { get; set; }
-    [StringLength(30)]
+    [StringLength(60)]
     [Display(Name = "Subject")] 
     public string? Speaker3Subject { get; set; }
     [Required]
-    [StringLength(30)]
+    [StringLength(60)]
     [Display(Name = "Closing Song")]
     public string? ClosingSong { get; set; }
     [Required]
-    [StringLength(40)]
+    [StringLength(60)]
     [Display(Name = "Closing Prayer")]
     public string? ClosingPrayer { get; set; }
 }
